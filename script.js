60779e59010a74fad2d428178712e8a0b6d99578
DIMITRIDAR <daraseliadimitri@gmail.com>
2024-05-23 01:33:55 +0400

    Write down Task 2 HTML and CSS inside HTML

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
-function changeText() {
-  let novel = document.getElementById("novel");
-  if (novel) {
-    novel.textContent = "Hello, DOM!";
-  } else {
-    console.error("Not Found!");
+// Task 1
+// In this task we have to create an HTML page with <p> element and write a JavaScript function that, when called,
+// changes
+// the text content of the <p> element to "Hello,DOM!"
+
+// function changeText() {
+//   let novel = document.getElementById("novel");
+//   if (novel) {
+//     novel.textContent = "Hello, DOM!";
+//   } else {
+//     console.error("Not Found!");
+//   }
+// }
+
+// changeText();
+
+//Task2
+// In this task we have to create an HTML page with a <button> element and write a JavaScript function that,
+//     when called, changes the background color of the <button> to red and the text color to white
+
+function changeColor() {
+  let search = document.getElementById("search");
+  if (search) {
   }
 }
-
-changeText();
