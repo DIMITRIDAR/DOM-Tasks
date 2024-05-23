5e535acf13dbb248eb1c79132e09392193e6c53e
DIMITRIDAR <daraseliadimitri@gmail.com>
2024-05-23 14:52:22 +0400

    Write down Task 4 description

--- script.js
+++ script.js
@@ -37,3 +37,7 @@
 // }
 
 // addParagraph();
+
+//Task 4
+//In this task we have to create an HTML page with a <button> element and write a JavaScript function that,
+//when the button is clicked, displays an alert with the message : "Button Clicked!"
